453c98b03d97c969933e846cd40c071b21cfd9a1
Umar Hassan Malik <58930229+UmarHassanMalik@users.noreply.github.com>
2020-10-06 15:52:21 +0500

    Update app.js

--- DockerCode/node-js-app/app.js
+++ DockerCode/node-js-app/app.js
@@ -5,7 +5,7 @@ const port = 8080
 const createdBy = process.env.CREATEDBY
 
 app.get('/', function(req, res){
- res.send('This is a example of NODE JS containerize app, created by ' + createdBy)
+ res.send('This is  NODE JS containerize app, created by ' + createdBy)
 })
 
 app.listen(port, () => {
